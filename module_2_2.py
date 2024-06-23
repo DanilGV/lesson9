0799d37c807612385d66d31ba3edb0bd448d13a4
DanilGV <danil.gareev.2015@mail.ru>
2024-06-23 05:40:47 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+a = float(input('Введите первое число :'))
+b = float(input('Введите второе число :'))
+c = float(input('Введите третье число :'))
+if a==b==c:
+    print(3)
+elif a==b or a==c or b==c:
+    print(2)
+else:
+    print(0)
